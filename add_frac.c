d5023f4141a532c1b7a59227510bee77495940ed
KaptainCS3 <mbiapplgryn@gmail.com>
2021-11-22 11:52:17 +0100

    Adding to fractions

---
+++ add_frac.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+    int num1, denom1, num2, denom2, result_num, result_denom;
+    printf("Enter first fraction: ");
+    scanf("%d/%d", &num1, &denom1);
+    printf("Enter second fraction: ");
+    scanf("%d/%d", &num2, &denom2);
+    result_num = num1 * denom2 + num2 * denom1;
+    result_denom = denom1 * denom2;
+    printf("The result is: %d/%d\n", result_num, result_denom); 
+}
