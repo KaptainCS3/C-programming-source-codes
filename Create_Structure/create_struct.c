587807c5d5894381dc34e63f2aca5211e9fd6825
KaptainCS3 <mbiapplegryn@gmail.com>
2022-01-18 21:39:57 +0100

    structure declaration

---
+++ Create_Structure/create_struct.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h> //contains dynamic allocation memory functions
+//declaring the structure syntax assign data to the node and pointer to the next node
+struct node{
+    int data;
+    struct node *next;
+};
+
