39a66822a3721a3d7a88e98dcf9189bd5bf49225
Leonard Appelgryn <70280902+KaptainCS3@users.noreply.github.com>
2022-03-31 19:51:00 +0100

    Add files via upload
    Factorial

---
+++ factorial.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+Int factorial (int z)
+{
+              Printf("error message");
+           }
+             break;
+Else
+           {
+              If((z==0) || (z==1)
+                {
+                    return 1;    
+                }
+else return z*factorial (z-1);
+                }
+            }
+    return 0;
+}
