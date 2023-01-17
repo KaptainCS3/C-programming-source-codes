4ad03d12e8c0cd84fe001cde88aba417435c04ae
KaptainCS3 <mbiapplegryn@gmail.com>
2023-01-17 23:40:44 +0100

    algo recursion problem

---
+++ recursion__.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+int one(int n);
+int main()
+{
+    one(5);
+    return 0;
+}
+int one(int n)
+{
+    if (n == 0)
+        printf("1 here represent n == 0 \n");
+    else
+    {
+        for (int i = 0; i < pow(2, n); i++)
+        {
+            printf("what is printed here???\n");
+            printf("%d\n", i);
+            one(n - 1);
+        }
+    }
+}
